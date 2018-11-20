988adc65ffe8cf8b700f8ddb891c6ca7c22bd777
Eric Chen <l0z9a@ugrad.cs.ubc.ca>
2018-11-20 13:03:33 -0800

    probably a better and space efficient solution

--- scripts/1_setup.sh
+++ scripts/1_setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir ~/Project_2
 mkdir ~/Project_2/1_Prokka_Output
+mkdir ~/Project_2/2_Concatenated_Prokka_Output
--- scripts/3_mass_cat.sh
+++ scripts/3_mass_cat.sh
@@ -7,12 +7,12 @@ for aa in ~/Project_2/1_Prokka_Output/$folder/*.faa;
 do
 	aa_file=$( basename $aa );
 	echo $aa_file
-	cat ~/Project_2/1_Prokka_Output/$folder/$aa_file >>~/Project_2/1_Prokka_Output/aa_orfs.faa
+	cat ~/Project_2/1_Prokka_Output/$folder/$aa_file >>~/Project_2/2_Concatenated_Prokka_Output/aa_orfs.faa
 done
 for nuc in ~/Project_2/1_Prokka_Output/$folder/*.ffn;
 do
 	nuc_file=$( basename $nuc );
 	echo $nuc_file
-	cat ~/Project_2/1_Prokka_Output/$folder/$nuc_file >>~/Project_2/1_Prokka_Output/nuc_orfs.ffn
+	cat ~/Project_2/1_Prokka_Output/$folder/$nuc_file >>~/Project_2/2_Concatenated_Prokka_Output/nuc_orfs.ffn
 done
 done
