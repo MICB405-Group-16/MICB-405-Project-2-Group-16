6555d5c2eaba3fa43360ffba96d520dc8047156a
Eric Chen <l0z9a@ugrad.cs.ubc.ca>
2018-11-23 14:49:00 -0800

    pathview now works

--- Transcriptome_RPKM.R
+++ Transcriptome_RPKM.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(ggplot2)
 library(pathview)
 
-si042_rpkm <- read_csv('4_RPKM_Output/SI042_150m_RPKM.csv', col_names=FALSE)
-si048_rpkm <- read_csv('4_RPKM_Output/SI048_150m_RPKM.csv', col_names=FALSE)
-si072_rpkm <- read_csv('4_RPKM_Output/SI072_150m_RPKM.csv', col_names=FALSE)
-si074_rpkm <- read_csv('4_RPKM_Output/SI074_150m_RPKM.csv', col_names=FALSE)
-si075_rpkm <- read_csv('4_RPKM_Output/SI075_150m_RPKM.csv', col_names=FALSE)
+si042_rpkm <- read_csv('4_RPKM_Output/SI042_150m_RPKM.csv', col_names=c("ID", "RPKM"))
+si048_rpkm <- read_csv('4_RPKM_Output/SI048_150m_RPKM.csv', col_names=c("ID", "RPKM"))
+si072_rpkm <- read_csv('4_RPKM_Output/SI072_150m_RPKM.csv', col_names=c("ID", "RPKM"))
+si074_rpkm <- read_csv('4_RPKM_Output/SI074_150m_RPKM.csv', col_names=c("ID", "RPKM"))
+si075_rpkm <- read_csv('4_RPKM_Output/SI075_150m_RPKM.csv', col_names=c("ID", "RPKM"))
 prokka_tsv <- read_tsv('2_Concatenated_Prokka_Output/all_tsvs.tsv', col_names=TRUE)
 prokka_mag_ref <- read_csv('2_Concatenated_Prokka_Output/mag_prokka_ref.csv', col_names=TRUE)
 kegg <- read_tsv('5_KEGG_Output/query.ko', col_names=c("ID", "KO"))
@@ -15,9 +15,8 @@ ar_gtdbtk <- read_tsv('0_Initial_Data/Bin_Taxonomy_Classification/gtdbtk.ar122.c
 
 clean_rpkm_data <- function(dataframe, cruise_name){
   clean_data <- dataframe %>%
-    mutate(Cruise = cruise_name) %>%
-    rename(ID=X1, RPKM=X2) %>%
-    inner_join(prokka_tsv, c("ID" = "locus_tag")) %>%
+    mutate(Cruise=cruise_name) %>%
+    inner_join(prokka_tsv, c("ID"="locus_tag")) %>%
     separate("ID", c("MAG_ID", "ORF_ID"), sep="_") %>%
     inner_join(prokka_mag_ref, c("MAG_ID"))
   return(clean_data)
@@ -48,7 +47,7 @@ full_table <- bind_rows(clean_rpkm_data(si042_rpkm, "SI042"),
           clean_rpkm_data(si072_rpkm, "SI072"),
           clean_rpkm_data(si074_rpkm, "SI074"),
           clean_rpkm_data(si075_rpkm, "SI075")) %>%
-  select(MAG_ID, ORF_ID, RPKM, Cruise, ftype, length_bp, gene, product, MAG_NUM) %>%
+  dplyr::select(MAG_ID, ORF_ID, RPKM, Cruise, ftype, length_bp, gene, product, MAG_NUM) %>%
   filter(RPKM > 0)
 
 #TODO: is this what we want? this inner join throws out about half the rows, because kegg has no idea what the ORF is
@@ -59,6 +58,18 @@ ultimate_table <- full_table %>%
 ultimate_table %>% View()
 
 rpkm_by_cruise_and_ko <- ultimate_table %>%
-  group_by(KO, MAG_NUM, Cruise) %>%
+  group_by(KO, MAG_NUM) %>%
   summarize(total_RPKM = sum(RPKM)) %>%
-  spread(key = MAG_NUM, value = total_RPKM)
+  spread(key = MAG_NUM, value = total_RPKM) %>%
+  filter(!is.na(KO))
+
+pv_mat <- dplyr::select(rpkm_by_cruise_and_ko, -KO)
+rownames(pv_mat) <- rpkm_by_cruise_and_ko$KO
+
+#sulfur = 00920
+#nitrogen = 00910
+#carbon = 01200
+pv.out <- pathview(gene.data = pv_mat,
+                   species = "ko",
+                   pathway.id="00920",
+                   kegg.dir = "6_Pathview/")
