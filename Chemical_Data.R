c0dc9436dc6d8f86e16c5686c02aab1401c1cab1
Eric Chen <l0z9a@ugrad.cs.ubc.ca>
2018-11-26 16:20:34 -0800

    namespace just for safety

--- Chemical_Data.R
+++ Chemical_Data.R
@@ -10,10 +10,10 @@ proj_dat <- raw_dat %>%
   mutate(Depth_m = Depth * 1000) %>%
   filter(Depth_m == 150) %>%
   rename(O2_uM=WS_O2, NO3_uM=WS_NO3, H2S_uM=WS_H2S, PO4_uM=WS_PO4) %>%
-  select(Date, Cruise, SI, O2_uM, PO4_uM, NO3_uM, H2S_uM, Mean_NH4, Mean_NO2, Mean_N2, Mean_O2, Mean_co2, Mean_N2O, Mean_CH4, Temperature, Salinity, Density)
+  dplyr::select(Date, Cruise, SI, O2_uM, PO4_uM, NO3_uM, H2S_uM, Mean_NH4, Mean_NO2, Mean_N2, Mean_O2, Mean_co2, Mean_N2O, Mean_CH4, Temperature, Salinity, Density)
 
 chemicals_of_interest <- proj_dat %>%
-  select(Date, O2_uM, NO3_uM, H2S_uM)
+  dplyr::select(Date, O2_uM, NO3_uM, H2S_uM)
 
 cruise_dates_mags <- proj_dat %>%
   filter(Cruise %in% c(42, 48, 72, 74, 75))
